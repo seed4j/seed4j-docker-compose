c814b4aabd6a7738046f26c2d48d35629e4bfe9d
Pascal Grimaud <pascalgrimaud@gmail.com>
2025-09-06 10:51:10 +0200

    Update scripts

--- status.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker ps -a --format "table {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}"
--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
 
+docker pull seed4j/seed4j:main
 docker compose up -d
 docker compose ps
